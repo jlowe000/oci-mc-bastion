48116f8b1976fe79513a0cfce124d378c7febbe3
Jason Lowe <jlowe000@gmail.com>
2021-08-17 21:44:12 +1000

    support using port forwarding sessions

--- create-bastion.sh
+++ create-bastion.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+RESOURCE_NAME=""
+RESOURCE_PORT="22"
+RESOURCE_IP_ADDR=""
+SESSION_NAME="mysession"
+
 # Include Environment Variables
 . ./mc-env.sh
 
-RESOURCE_PORT="22"
-SESSION_NAME="mysession"
-
 # Get the options
-while getopts ":hb:c:s:r:p:i:" option; do
+while getopts ":hb:c:s:r:p:a:i:" option; do
   case $option in
     h) # display Help
-       echo "-b <bastion_name> -c <compartment_name> -s <subnet_name> -r <resource_name> -p <port> -i <session_name>"
+       echo "-b <bastion_name> -c <compartment_name> -s <subnet_name> -r <resource_name> -a <ip_address> -p <port> -i <session_name>"
        exit;;
     b) # Set Bastion (by arg)
        BASTION_NAME=$OPTARG;;
@@ -20,6 +22,8 @@ while getopts ":hb:c:s:r:p:i:" option; do
        SUBNET_NAME=$OPTARG;;
     r) # Set Resource (by arg)
        RESOURCE_NAME=$OPTARG;;
+    a) # Set Resource IP Address (by arg)
+       RESOURCE_IP_ADDR=$OPTARG;;
     p) # Set Resource Port (by arg)
        RESOURCE_PORT=$OPTARG;;
     i) # Set Session Name ie (ID) (by arg)
@@ -41,8 +45,10 @@ COMPARTMENT_OCID=`oci --profile ${PROFILE_NAME} iam compartment list --all --nam
 echo ${COMPARTMENT_OCID}
 SUBNET_OCID=`oci --profile ${PROFILE_NAME} network subnet list --all --display-name "${SUBNET_NAME}" --compartment-id ${COMPARTMENT_OCID} | jq -r ".data[0].id"` 
 echo ${SUBNET_OCID}
-RESOURCE_OCID=`oci --profile ${PROFILE_NAME} compute instance list --all --display-name "${RESOURCE_NAME}" --compartment-id ${COMPARTMENT_OCID} | jq -r ".data[0].id"` 
-echo ${RESOURCE_OCID}
+if [ "" != "${RESOURCE_NAME}" ]; then
+  RESOURCE_OCID=`oci --profile ${PROFILE_NAME} compute instance list --all --display-name "${RESOURCE_NAME}" --compartment-id ${COMPARTMENT_OCID} | jq -r ".data[0].id"` 
+  echo ${RESOURCE_OCID}
+fi
 
 # Create Bastion Instance if required
 LIST_BASTION=`oci --profile ${PROFILE_NAME} bastion bastion list --all --compartment-id ${COMPARTMENT_OCID} --name ${BASTION_NAME} --bastion-lifecycle-state ACTIVE`
@@ -70,7 +76,11 @@ echo ${BASTION_OCID}
 # Create Bastion Session if required
 LIST_SESSION=`oci --profile ${PROFILE_NAME} bastion session list --all --bastion-id ${BASTION_OCID} --session-lifecycle-state ACTIVE`
 if [ "" = "${LIST_SESSION}" ]; then
-  SESSION_WR=`oci --profile ${PROFILE_NAME} bastion session create-managed-ssh --bastion-id ${BASTION_OCID} --ssh-public-key-file ${SSH_KEY_PUB} --target-os-username opc --target-port 22 --target-resource-id ${RESOURCE_OCID}`
+  if [ "" != "${RESOURCE_OCID}" ]; then
+    SESSION_WR=`oci --profile ${PROFILE_NAME} bastion session create-managed-ssh --bastion-id ${BASTION_OCID} --ssh-public-key-file ${SSH_KEY_PUB} --target-os-username opc --target-port 22 --target-resource-id ${RESOURCE_OCID}`
+  else
+    SESSION_WR=`oci --profile ${PROFILE_NAME} bastion session create-port-forwarding --bastion-id ${BASTION_OCID} --ssh-public-key-file ${SSH_KEY_PUB} --target-private-ip ${RESOURCE_IP_ADDR}  --target-port ${RESOURCE_PORT}`
+  fi
   until [ "" != "${LIST_SESSION}" ]
   do
     LIST_SESSION=`oci --profile ${PROFILE_NAME} bastion session list --all --bastion-id ${BASTION_OCID} --session-lifecycle-state ACTIVE`
@@ -93,11 +103,7 @@ PRIVATE_IP_ADDRESS=`echo ${GET_SESSION} | jq -r ".data.\"target-resource-details
 echo ${PRIVATE_IP_ADDRESS}
 
 # Create SSH Tunnel for Minecraft
-SSH_CMD=`echo ${GET_SESSION} | jq -r ".data.\"ssh-metadata\".command" | sed -E 's/<privateKey>/${SSH_KEY_PRIV}/g'`
-
-if [ "22" != "${RESOURCE_PORT}" ]; then
-  SSH_CMD=`echo ${SSH_CMD} | sed -E 's/-p 22/-L ${RESOURCE_PORT}:${PRIVATE_IP_ADDRESS}:${RESOURCE_PORT} -N/g'`
-fi
+SSH_CMD=`echo ${GET_SESSION} | jq -r ".data.\"ssh-metadata\".command" | sed -E 's/<privateKey>/${SSH_KEY_PRIV}/g' | sed -E 's/<localPort>/${RESOURCE_PORT}/g'`
 
 echo ${SSH_CMD}
 eval ${SSH_CMD}
